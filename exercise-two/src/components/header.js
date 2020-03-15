81518f0a6dbcb39cf8048fef38e7053d08a7f98c
Clara <cg2984@nyu.edu>
2020-03-14 22:58:16 -0400

    added cities i wanted

--- exercise-two/src/components/header.js
+++ exercise-two/src/components/header.js
@@ -5,9 +5,9 @@ function Header(){
 		<header className = "Header">
 			<h1>Weather Application</h1>
 			<nav className = "CityNav">
-				<a className = "City" href="/?city=toronto">Toronto</a>
+				<a className = "City" href="/?city=oslo">Oslo</a>
 				<a className = "City" href="/?city=Seoul">Seoul</a>
-				<a className = "City" href="/?city=Miami">Miami</a>
+				<a className = "City" href="/?city=Melbourne">Melbourne</a>
 				<a className = "City" href="/?city=oaxaca">Oaxaca</a>
 			</nav>
 		</header>
